20f1826bf4b58ef824931505d56d923f1a3917ba
AmitKumar1827 <amit.rajput.1827@gmail.com>
2022-03-23 10:55:33 +0530

    first commit

---
+++ src/main/java/com/example/project/Controller/UserController.java
@@ -0,0 +1,42 @@
+package com.example.project.Controller;
+
+
+import com.example.project.Entity.User;
+import com.example.project.Services.UserServices;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import javax.validation.Valid;
+
+import java.util.List;
+
+@RestController
+@RequestMapping
+public class UserController {
+
+    @Autowired
+    UserServices userServices;
+
+    @GetMapping("/userDetails")
+    public User getUserDetails( @RequestParam(required = true) Integer id){
+        return userServices.getUserDetails(id);
+    }
+
+    @PostMapping("/addUser")
+    public void addUser(@RequestBody @Valid User user){
+
+       userServices.addUser(user);
+    }
+
+    @PutMapping("/updateUser")
+    public User updateUser(@RequestBody @Valid User user){
+
+
+        User user1 = userServices.updateUser(user);
+        return user1;
+    }
+
+
+
+
+
+}
